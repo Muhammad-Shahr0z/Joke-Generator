164660dfa6ef974287e898a027ba20303ea7f6fc
MuhammadShahroz <159715083+Muhammad-Shahr0z@users.noreply.github.com>
2025-03-08 22:57:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import jokes
 from fastapi import FastAPI
+import streamlit as st
 
 app = FastAPI()
 
 @app.get("/jokes")
 def get_jokes():
     return {"jokes": jokes.tech_jokes}
+
+
+st.title("Tech Jokes API")
+st.write("Here are some tech jokes:")
+for joke in jokes.tech_jokes:
+    st.write(joke)
+
+
 
 
 
